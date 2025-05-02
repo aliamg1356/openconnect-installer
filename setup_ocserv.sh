48fb754cd865666d5caf9ddcc195639ed487262c
aliamg1356 <124570423+aliamg1356@users.noreply.github.com>
2025-05-02 18:25:45 +0330

    Update setup_ocserv.sh

--- setup_ocserv.sh
+++ setup_ocserv.sh
@@ -32,7 +32,7 @@ VPN_PORT=$(whiptail --title " VPN Port" --inputbox "Enter VPN port number (defau
 
 # == Obtain SSL Certificate ==
 echo "[✔] Getting SSL certificate from Let's Encrypt for $DOMAIN..."
-certbot certonly --standalone -d "$DOMAIN" --agree-tos -n --email "$EMAIL" --http-01-port=8080
+certbot certonly --standalone -d "$DOMAIN" --agree-tos -n --email "$EMAIL"
 if [ $? -ne 0 ]; then
   whiptail --title "❌ Error" --msgbox "Failed to get SSL certificate. Exiting." 10 50
   exit 1
